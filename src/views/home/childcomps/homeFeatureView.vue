9d121b4af61ba7b8f98c43ddf5a39165cd84f1e4
梁 <yitu9132511@163.com>
2020-02-26 20:54:16 +0800

    结构修改

---
+++ src/views/home/childcomps/homeFeatureView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="feature">
+      <a href="act.mogujie.com/zzlx67">
+        <img src="~assets/img/home/recommend_bg.jpg" alt="">
+      </a>
+  </div>
+</template>
+
+<script>
+export default {
+  components:{},
+  props:{},
+  data(){
+    return {
+    }
+  },
+  watch:{},
+  computed:{},
+  methods:{},
+  created(){},
+  mounted(){}
+}
+</script>
+<style lang="less" scoped>
+  .feature{
+      img{width:100vw;}
+  }
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -5,8 +5,41 @@
     </nav-bar>
     <home-swiper :banners="banners"/>
     <home-recommend-view :recommends="recommends"/>
+    <home-feature-view></home-feature-view>
     <h1>首页</h1>
 
+    <ul>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+      <li>列表11</li>
+      <li>列表12</li>
+      <li>列表13</li>
+      <li>列表14</li>
+      <li>列表15</li>
+      <li>列表16</li>
+      <li>列表17</li>
+      <li>列表18</li>
+      <li>列表19</li>
+      <li>列表20</li>
+      <li>列表21</li>
+      <li>列表22</li>
+      <li>列表23</li>
+      <li>列表24</li>
+      <li>列表25</li>
+      <li>列表26</li>
+      <li>列表27</li>
+      <li>列表28</li>
+      <li>列表29</li>
+      <li>列表30</li>
+    </ul>
     
   </div>
 </template>
@@ -15,6 +48,7 @@
 import navBar from 'components/common/navbar/NavBar.vue';
 import homeSwiper from './childcomps/homeSwiper';
 import homeRecommendView from './childcomps/homeRecommendView';
+import homeFeatureView from './childcomps/homeFeatureView';
 
 import {getHomeMultidata} from "network/home";
 //插件引入swiper
@@ -22,7 +56,7 @@ import {getHomeMultidata} from "network/home";
 export default {
   name:"home",
   components:{
-    navBar,homeSwiper,homeRecommendView
+    navBar,homeSwiper,homeRecommendView,homeFeatureView
     /* swiper */
   },
   props:{},
@@ -44,8 +78,14 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+  #home{
+    padding-top:44px;
+  }
   .home-nav{
     background-color: var(--color-tint);
     color:white;
+    position: fixed;
+    left:0;top:0;right:0;
+    z-index: 100;
   }
 </style>
